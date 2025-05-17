ebf94305ae4d35abef3fea1a253c1922f0f969d7
Trong Truong <trongtruong2509@gmail.com>
2025-05-17 08:53:53 +0700

    test with podinfo first

--- manual_install.sh
+++ manual_install.sh
@@ -4,5 +4,5 @@ flux install
 
 flux create source git flux-system \
   --url=https://github.com/trongtruong2509/fleet-infra \
-  --branch=master \
-  --interval=1m \
+  --branch=adopt-test \
+  --interval=1m 
